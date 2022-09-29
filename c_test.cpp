b70a952e33d805c1a6b0aa55c4f06fdda554fcd4
HsinYehHsu <72120272+hsuhe99@users.noreply.github.com>
2022-09-29 09:49:47 +0800

    test code

---
+++ c_test.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int fun(int n, int* f_p) {
+    int t, f;
+    if (n <= 1) {
+        *f_p = 1;
+        return 1;
+    }
+    t = fun(n - 1, f_p);
+    f = t + *f_p;
+    *f_p = t;
+    printf("%d %d\n", f,t );
+    return f;
+}
+
+int main() {
+    int x = 5;
+    printf("%d\n", fun(10, &x));
+    return 0;
+}
